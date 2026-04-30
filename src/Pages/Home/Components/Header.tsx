ea25c5ecef4e401d403cc4a49c7b9ca9dd00f67a
ThibNach <thibaut.spreux@gmail.com>
2026-04-30 09:09:58 +0200

    update resume and site title

--- src/Pages/Home/Components/Header.tsx
+++ src/Pages/Home/Components/Header.tsx
@@ -4,7 +4,7 @@ const Hero_Header = () => {
     return (
         <header className="py-5 mb-5 text-center bg-p">
             <h1 className="display-3 fw-bold">Thibaut Spreux</h1>
-            <p className="lead">Game Developer</p>
+            <p className="lead">Software Developer</p>
             <Link to="/portfolio" className="btn btn-primary btn-lg mt-3">
                 Explore My Work
             </Link>
